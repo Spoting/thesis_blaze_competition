26d719cbd657b8d78836ec0add994a0324060de9
Spoting <ap.spiros00@gmail.com>
2025-05-26 21:23:26 +0300

    Install Twig + Minor Test Files

--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Routing\Requirement\Requirement;
 
@@ -12,8 +13,9 @@ use Symfony\Component\Routing\Requirement\Requirement;
 final class TestController extends AbstractController
 {
     #[Route('/test/{id}', name: 'app_test',  requirements: ['id' => Requirement::DIGITS])]
+    #[Route('/test_alias/{id}', name: 'app_alias_route')]
     public function index(int $id): JsonResponse
-    {
+    {   
         return $this->json([
             'message' => 'Welcome to your new controller! --- ' . $id,
             'path' => 'src/Controller/TestController.php',
@@ -22,11 +24,14 @@ final class TestController extends AbstractController
 
 
     #[Route('/test/{slug}', name: 'app_test_test')]
-    public function show(string $slug): JsonResponse
+    public function show(string $slug): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller! slagki --- ' . $slug,
-            'path' => 'src/Controller/TestController.php',
+        // return $this->json([
+        //     'message' => 'Welcome to your new controller! slagki --- ' . $slug,
+        //     'path' => 'src/Controller/TestController.php',
+        // ]);
+        return $this->render('test.html.twig', [
+            'body' => $slug,
         ]);
     }
 }
