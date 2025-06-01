a9351e6d759dbc6e88fe0385fec31a6e74d97e62
Spoting <ap.spiros00@gmail.com>
2025-06-01 18:31:12 +0300

    feat: Adds Competition Form + Twigs for Competition Items

--- src/Controller/Public/PublicCompetitionController.php
+++ src/Controller/Public/PublicCompetitionController.php
@@ -1,11 +1,13 @@
-<?php 
+<?php
 
 // src/Controller/Public/PublicCompetitionController.php
 namespace App\Controller\Public;
 
 use App\Entity\Competition;
+use App\Form\Public\SubmissionType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -17,7 +19,8 @@ class PublicCompetitionController extends AbstractController
         $now = new \DateTimeImmutable();
         $competitions = $entityManager->getRepository(Competition::class)
             ->createQueryBuilder('c')
-            ->where('c.endDate > :now')
+            ->where('c.startDate <= :now')
+            ->andWhere('c.endDate > :now')
             ->setParameter('now', $now)
             ->orderBy('c.endDate', 'ASC')
             ->getQuery()
@@ -25,6 +28,40 @@ class PublicCompetitionController extends AbstractController
 
         return $this->render('public/competitions.html.twig', [
             'competitions' => $competitions,
+        ]);
+    }
+
+
+
+    #[Route('/competition/{id}/submit', name: 'public_submit_form')]
+    public function submitForm(int $id, EntityManagerInterface $entityManager, Request $request): Response
+    {
+        $competition = $entityManager->getRepository(Competition::class)->find($id);
+
+        if (
+            !$competition instanceof Competition
+            || $competition->getEndDate() < new \DateTimeImmutable()
+        ) {
+            throw $this->createNotFoundException('Competition not found or has ended.');
+        }
+
+        $form = $this->createForm(SubmissionType::class, null, [
+            'competition_id' => $id,
+            'form_fields' => $competition->getFormFields(),
+        ]);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $formData = $form->getData();
+            // Include competition ID in the submitted data
+            $submissionData = array_merge(['competition_id' => $id], $formData);
+            dump($submissionData); // TODO: Process the form submission (Redis, RabbitMQ)
+        }
+
+        return $this->render('public/submit_form.html.twig', [
+            'competition' => $competition,
+            'form' => $form->createView(),
         ]);
     }
 }
---
+++ src/Form/Public/SubmissionType.php
@@ -0,0 +1,60 @@
+<?php
+
+// src/Form/Public/SubmissionType.php
+namespace App\Form\Public;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TelType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SubmissionType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $formFieldsConfig = $options['form_fields'] ?? [];
+
+        $builder
+            ->add('competition_id', HiddenType::class, [
+                'mapped' => false,
+                'data' => $options['competition_id'],
+            ]);
+
+        foreach ($formFieldsConfig as $fieldConfig) {
+            $fieldName = $fieldConfig['name'] ?? null;
+            $fieldType = $fieldConfig['type'] ?? 'text';
+            $fieldLabel = $fieldConfig['label'] ?? $fieldName;
+            $fieldOptions = ['label' => $fieldLabel];
+
+            switch ($fieldType) {
+                case 'email':
+                    $builder->add($fieldName, EmailType::class, $fieldOptions);
+                    break;
+                case 'tel':
+                    $builder->add($fieldName, TelType::class, $fieldOptions);
+                    break;
+                // TODO: Add More.
+                default:
+                    $builder->add($fieldName, TextType::class, $fieldOptions);
+                    break;
+            }
+        }
+
+        $builder->add('submit', SubmitType::class, ['label' => 'Υποβολή Συμμετοχής']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'competition_id' => null,
+            'form_fields' => [],
+        ]);
+        $resolver->setRequired(['competition_id', 'form_fields']);
+        $resolver->setAllowedTypes('competition_id', 'int');
+        $resolver->setAllowedTypes('form_fields', 'array');
+    }
+}
